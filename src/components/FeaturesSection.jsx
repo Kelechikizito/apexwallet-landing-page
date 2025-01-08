759b5859a1f573a5632b4bd38f30ee80f3994a21
Kelechikizito <kaylaychi77@gmail.com>
2025-01-08 07:57:37 +0100

    files update

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -1,6 +1,12 @@
+import spanPill from "../assets/span-pill/Variant-1.png";
+import squareBackground from "../assets/backgrounds/Squares-Background.png";
+
+
 const FeaturesSection = () => {
   return (
-    <div>FeaturesSection</div>
+    <div>
+        <img src={spanPill} alt="" />       
+    </div>
   )
 }
 
