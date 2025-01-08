730ee0033f39e4101d30357fa8089d6e32dbfb3c
Kelechikizito <kaylaychi77@gmail.com>
2025-01-08 08:36:44 +0100

    files update

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -1,13 +1,41 @@
 import spanPill from "../assets/span-pill/Variant-1.png";
 import squareBackground from "../assets/backgrounds/Squares-Background.png";
 
-
 const FeaturesSection = () => {
   return (
-    <div>
-        <img src={spanPill} alt="" />       
-    </div>
-  )
-}
+    <div className="mt-24 mx-8 flex flex-col gap-8">
+      <div
+        className="flex flex-col items-center justify-center text-center gap-12 py-8"
+        style={{
+          backgroundImage: `url(${squareBackground})`,
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+      >
+        <div>
+          <img src={spanPill} alt="" />
+        </div>
 
-export default FeaturesSection
+        <div>
+          <h1 className="text-7xl font-bold text-white">
+            Key Features <br /> for Your Wallet
+          </h1>
+        </div>
+
+        <div>
+          <p className="text-white text-lg opacity-90">
+            Unlocking the Power of Our Wallet: Key <br /> Features Designed to
+            Enhance Your Crypto <br /> Experience
+          </p>
+        </div>
+      </div>
+
+      <div>
+        I am going to be a billionaire, a millionaire in the next six months
+      </div>
+    </div>
+  );
+};
+
+export default FeaturesSection;
